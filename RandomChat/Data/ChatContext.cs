f72473c8aa8fa23a782e638ec21f7c167f8df0f9
UoooBarry <37783883+UoooBarry@users.noreply.github.com>
2020-02-13 13:26:19 +0800

    Added Context

--- RandomChat/Data/ChatContext.cs
+++ RandomChat/Data/ChatContext.cs
@@ -14,13 +14,19 @@ namespace RandomChat.Data
 
         public DbSet<AppUser> Appusers { get; set; }
         public DbSet<Message> Messages{ get; set; }
+        public DbSet<Login> Logins { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder) //validation
         {
             base.OnModelCreating(builder);
+            builder.Entity<Login>().HasCheckConstraint("CH_Login_LoginID", "len(LoginID) = 8").
+                HasCheckConstraint("CH_Login_PasswordHash", "len(PasswordHash) = 64");
 
             builder.Entity<Message>().
                 HasOne(e => e.AppUser).WithMany(e => e.Messages).HasForeignKey(e => e.UsrID);
+
+            builder.Entity<Login>().
+                HasOne(e => e.AppUser).WithOne(e => e.Login);
         }
     }
 }
--- RandomChat/Startup.cs
+++ RandomChat/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RandomChat.Data;
 
 namespace RandomChat
 {
@@ -24,6 +26,13 @@ namespace RandomChat
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<ChatContext>(options => //add connection string
+            {
+                options.UseSqlServer(Configuration.GetConnectionString(nameof(ChatContext)));
+                // Enable lazy loading.
+                options.UseLazyLoadingProxies();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
