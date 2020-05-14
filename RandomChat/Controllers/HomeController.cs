fc65fa5c81071ed9ffa5294c4148b9195045a30c
noobfromvn99 <s3673712@student.rmit.edu.au>
2020-05-14 23:12:02 +1000

    add email send

--- RandomChat/Controllers/HomeController.cs
+++ RandomChat/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Amazon;
+using Amazon.SimpleEmail;
+using Amazon.SimpleEmail.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -14,8 +17,27 @@ namespace RandomChat.Controllers
 {
     public class HomeController : Controller
     {
+        // ConfigurationSetName = configSet argument below. 
         
+        static readonly string senderAddress = "bach.rmit.5499@gmail.com";
+        static readonly string subject = "Amazon SES test (AWS SDK for .NET)";
         private readonly ChatContext _context;
+
+        static readonly string textBody = "Amazon SES Test (.NET)\r\n"
+                                       + "This email was sent through Amazon SES "
+                                       + "using the AWS SDK for .NET.";
+        // The HTML body of the email.
+        static readonly string htmlBody = @"<html>
+<head></head>
+<body>
+  <h1>Amazon SES Test (AWS SDK for .NET)</h1>
+  <p>This email was sent with
+    <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
+    <a href='https://aws.amazon.com/sdk-for-net/'>
+      AWS SDK for .NET</a>.</p>
+</body>
+</html>";
+
 
         public HomeController(ChatContext context)
         {
@@ -62,18 +84,60 @@ namespace RandomChat.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateAsync(String email, String password)
         {
-            
+            using (var client = new AmazonSimpleEmailServiceClient(RegionEndpoint.USEast1))
+            {
+                var sendRequest = new SendEmailRequest
+                {
+                    Source = senderAddress,
+                    Destination = new Destination
+                    {
+                        ToAddresses =
+                        new List<string> { email }
+                    },
+                    Message = new Message
+                    {
+                        Subject = new Content(subject),
+                        Body = new Body
+                        {
+                            Html = new Content
+                            {
+                                Charset = "UTF-8",
+                                Data = htmlBody
+                            },
+                            Text = new Content
+                            {
+                                Charset = "UTF-8",
+                                Data = textBody
+                            }
+                        }
+                    },
+                    
+                };
+                try
+                {
+                    
+                    var response = client.SendEmailAsync(sendRequest);
+                    ViewBag.Message = String.Format("Send Email successfully");
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Message = String.Format("Send Email fails: " + ex.Message);
+                   
+                }
+            }
             Login login = new Login { Email = email,  PasswordHash = PBKDF2.Hash(password) };
             _context.Logins.Add(login);
             AppUser user = new AppUser { Email = login.Email };
             _context.Appusers.Add(user);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            return View();
         }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        
     }
 }
