0c52ff39c6d1364451210fb9342b686274491c5a
HuahuaRen <huahua.personal@gmail.com>
2020-05-16 16:25:32 +0800

    added ajax to reload page

--- RandomChat/Models/Topic.cs
+++ RandomChat/Models/Topic.cs
@@ -1,19 +1,30 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using DynamoDb.libs.DynamoDb;
+using DynamoDB.Models;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace RandomChat.Models
 {
     public class Topic
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity), Key]
-        public int TopicId { get; set; }
+        public int? TopicId { get; set; }
 
         [Required]
         public string TopicName { get; set; }
 
         [Required]
         [ForeignKey("AppUser")]
-        public int UserId { get; set; }
-        public virtual AppUser creator { get; set; }
+        public int? UserId { get; set; }
+        public virtual AppUser User { get; set; }
+
+        public string FirstReplyAsync() 
+        {
+            return "Anonymous Replies";
+        }
+
+     
     }
 }
