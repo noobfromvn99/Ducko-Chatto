b1b4fa34c95b59b3d48a6e6ca5ce6d174eb12ff3
noobfromvn99 <s3673712@student.rmit.edu.au>
2020-05-19 01:22:07 +1000

    add change

--- RandomChat/Controllers/ChatController.cs
+++ RandomChat/Controllers/ChatController.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using RandomChat.Data;
 using RandomChat.Models;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace RandomChat.Controllers
 {
@@ -12,11 +17,16 @@ namespace RandomChat.Controllers
     {
         private readonly ChatContext _context;
         private ChatManger chatManger;
-
+        private const string bucketName = "ducko-chatto-image-store";
+        private const string keyName = "image";
+        private const string filePath = "C:\\Users\\Bach Rach\\Desktop\\79934978_2536034256721619_1821982627985358848_o.jpg";
+        private static readonly RegionEndpoint bucketRegion = RegionEndpoint.USEast1;
+        private static IAmazonS3 s3Client;
         public ChatController(ChatContext context)
         {
             _context = context;
             chatManger = ChatManger.getInstance();
+            s3Client = new AmazonS3Client(bucketRegion);
         }
 
         public IActionResult Index()
@@ -62,8 +72,23 @@ namespace RandomChat.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(string TopicName)
+        public async Task<IActionResult> CreateAsync(string TopicName)
         {
+            try
+            {
+                var fileTransferUtility = new TransferUtility(s3Client);
+                await fileTransferUtility.UploadAsync(filePath, bucketName);
+            }
+            catch (AmazonS3Exception e)
+            {
+                ModelState.AddModelError("Error", "Error encountered on server. Message: " + e.Message + " when writing an object");
+            }
+            catch (Exception e)
+            {
+                ModelState.AddModelError("Error", "Unknown encountered on server. Message: "+ e.Message + " when writing an object" );
+            
+            }
+
             int UserId = Convert.ToInt32(HttpContext.Session.GetInt32(nameof(AppUser.UserID)));
             string location = HttpContext.Session.GetString("city");
             if (UserId == 0 || TopicName == null)
